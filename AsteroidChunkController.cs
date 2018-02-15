ac3f914904ec1358ad28897613550dbfaad53788
Cheney Ni <cheney.ni@ccs.us>
2018-02-15 07:44:16 -0500

    testing another commit

--- AsteroidChunkController.cs
+++ AsteroidChunkController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 // using System.Collections.Generic;
 using UnityEngine;
+//random comment
 public class AsteroidChunkController : MonoBehaviour {
 
 	// Public variables:
