63e34a64915585abb3e20ac5ba2e49c104077dac
Cheney Ni <cheney.ni@ccs.us>
2018-02-14 23:51:27 -0500

    final final commit

--- ShieldIconAdjust.cs
+++ ShieldIconAdjust.cs
@@ -1,6 +1,7 @@
 ﻿// using System.Collections;
 // using System.Collections.Generic;
 using UnityEngine;
+//new comment
 public class ShieldIconAdjust : MonoBehaviour {
 
 	// Use this for initialization
